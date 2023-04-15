8efaf206e887448fd353b3a72cdea670d8ead547
philippe goulliart <philippe.goulliart62@gmail.com>
2023-04-15 11:04:25 +0200

    prrecompiling pb not fixxed

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module AnimeList
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.assets false
+      generate.helper false
+      generate.test_framework :test_unit, fixture: false
+    end
+    config.assets.enabled = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
